9a7222f53a81ffde4958711cb68804c54fa93590
Patryk Woziński <patryk.wozinski@gmail.com>
2018-11-21 20:19:04 +0100

    Basic init

---
+++ src/Freeq/PhpRetrier/Retrier.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freeq
+ * Date: 21/11/2018
+ * Time: 20:18
+ */
+declare(strict_types=1);
+
+namespace Freeq\PhpRetrier;
+
+
+final class Retrier
+{
+}
---
+++ tests/RetrierTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freeq
+ * Date: 21/11/2018
+ * Time: 20:13
+ */
+declare(strict_types=1);
+
+namespace Freeq\PhpRetrier\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class RetrierTest extends TestCase
+{
+    public function test_x(): void
+    {
+        $this->assertTrue(true);
+    }
+}
